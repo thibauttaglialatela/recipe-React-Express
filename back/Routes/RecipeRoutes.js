c0d07409cea04321a52ebeba0e4783bda296e8dd
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-10-11 10:12:16 +0200

    📦 NEW: récupérer les détails d'une recette

--- back/Routes/RecipeRoutes.js
+++ back/Routes/RecipeRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createRecipe, getAllRecipes } = require("../Controllers/RecipeController");
+const { createRecipe, getAllRecipes, getOneRecipeById } = require("../Controllers/RecipeController");
 const router = express.Router();
 
 router.post('/api/recipes/', createRecipe);
 router.get('/api/recipes/list', getAllRecipes);
+router.get('/api/recipes/:id', getOneRecipeById);
 
 module.exports = router;
